68a2ff91080dee8a9c10c1d5da53a994a6f657a9
Andy Hefner <ahefner@gmail.com>
2012-01-27 15:58:45 -0500

    Minor tweaks.

--- hacks/ryden.lisp
+++ hacks/ryden.lisp
@@ -1,3 +1,23 @@
+;;;; This is a very simple graphics demo. It scrolls horizontally a
+;;;; portion of some artwork by Mark Ryden, in various colors.  This
+;;;; is converted from a GIF file to an 8 KB character ROM during
+;;;; assembly. The image is large enough that we need some timed code
+;;;; to switch pattern table banks mid-frame.  It also incorporates an
+;;;; NSF file to provide some simple music.
+
+;;;; The music source (noisejam.mml) was compiled to an NSF externally
+;;;; using MCK and NESASM. In theory you could swap other NSFs in
+;;;; place of this one, provided you fix the player addresses
+;;;; (music-init and music-step) to match the file header, and no
+;;;; variables clash in the zero page.
+
+;;;; It targets a basic NROM board (32KB program, 8KB character). I
+;;;; haven't had a chance to test on real hardware yet.
+
+;;;; Somewhat embarrassingly, this demo reveals a bug in my own NES
+;;;; emulator which I've yet to fix, where the frame timing is
+;;;; apparently off by one scanline. Oops.
+
 (defpackage :ryden-demo
   (:use :common-lisp :6502 :6502-modes :asm6502 :asm6502-utility :asm6502-nes))
 
@@ -57,34 +77,34 @@
                      #x3F #x2D #x3D #x30  #x3F #x03 #x13 #x23))
       (poke color +vram-io+))
 
-    (lda (imm 64))
+    (lda (imm 64))                      ; Initialize scroll variables
     (sta scroll-x)
     (lda (imm 0))
     (sta ntaddr)
+    (lda (imm 160))
+    (sta phase)
+
     (align 256 #xEA)                    ; Pad with NOPs to next page
     (poke #b10001000 +ppu-cr1+)         ; Enable NMI
 
     (with-label mainloop
       (jsr 'wait-for-vblank)
 
-      (lda (mem +ppu-status+))          ; Set scroll register
+      (lda (mem +ppu-status+))          ; Reset address latch
 
-      (ldx phase)
+      (ldx phase)                       ; 'phase' steps through rate-pattern
       (inx)
       (stx phase)
       (lda scroll-x)                    ; Scroll horizontally..
-      (clc)                             ; Increment scroll-x
-;      (adc (imm 1))
-      (adc (abx 'rate-pattern))
-
-
-      (sta (mem +vram-scroll+))
+      (clc)                             ; Update scroll-x by rate-pattern
+      (adc (abx 'rate-pattern))         ; Add. Use the carry-out below!
       (sta scroll-x)
+      (sta (mem +vram-scroll+))         ; Set scroll registers
       (lda ntaddr)                      ; Carry into ntaddr
-      (adc (imm 0))
+      (adc (imm 0))                     ; ** Carry in from ADC above. **
       (anda (imm 1))                    ; Carry toggles $2000/$2400
       (sta ntaddr)
-      (ora (imm #b10001000))            ; NMI on, BG Patterns $0000
+      (ora (imm #b10001000))            ; NMI on, BG Pattern table $0000
       (sta (mem +ppu-cr1+))
       (poke #b00001110 +ppu-cr2+)       ; BG on, sprites off.
       (lda (imm 0))
@@ -92,22 +112,21 @@
       ;; Switch pattern tables mid-frame:
       (emit-delay (+ (* 114 146) 178))
       (lda ntaddr)
-      (ora (imm #b10011000))            ; NMI on, BG Patterns $1000
+      (ora (imm #b10011000))            ; NMI on, BG Pattern table $1000
       (sta (mem +ppu-cr1+))
 
       (jsr music-step)
       (jmp (mem 'mainloop))))
 
+  ;; This table controls the scrolling.
   (with-label rate-pattern
-    (let* ((tr '(1 0 0 0 1 0 0 1 0 1 0 1 01))
-           (motion (concatenate 'list
-                                (loop repeat (- 128 (reduce '+ tr))
-                                      collect 1)
-                                (reverse tr)))
-           (pattern (concatenate 'list
-                                 motion
-                                 (loop repeat (- 256 (length motion))
-                                       collect 0))))
+    (let* ((tr '(1 0 1 0 1 0 1 0 0 1 0 0 1 0 0 0 1))
+           (pattern (subseq
+                     (append (loop repeat (- 128 (reduce '+ tr))
+                                   collect 1)
+                             tr
+                             (loop repeat 256 collect 0))
+                     0 256)))
       (assert (= 256 (length pattern)))
       (assert (= 128 (reduce #'+ pattern)))
       (map nil 'db pattern)))
@@ -131,6 +150,7 @@
         (dey))
       (dec fill-count)))
 
+  ;; Wasteful, but the ROM is mostly empty anyway.
   (procedure fill-attributes
     (dotimes (y 8)
       (dotimes (x 4) (sty (mem +vram-io+)))
