f6604b7a3dca6bd752528f47ea457ecd762fc604
Andy Hefner <ahefner@gmail.com>
2016-04-24 20:09:03 -0400

    More sprite test fun

--- hacks/test1/sprite-test.lisp
+++ hacks/test1/sprite-test.lisp
@@ -18,7 +18,8 @@
 
 ;;;; Globals
 
-
+(defparameter status-update-vector (wordvar #xE0)
+  "Status bar updates by END-FRAME are done by setting this code pointer.")
 
 (defparameter *sprite-x* (zp #xF0))
 (defparameter *sprite-y* (zp #xF1))
@@ -29,6 +30,8 @@
 
 (defparameter *frame-counter* (zp #xFD)
   "Incremented each frame by NMI handler")
+
+;;(defparameter *sprite-count* (zp #xFD))
 (defparameter *oamidx* (zp #xFE))
 (defparameter vblank-flag (zp #xFF))
 
@@ -75,7 +78,13 @@
           (sta (aby *oam-shadow*))
           (iny)))
       ',body)
-     (sty *oamidx*)
+     ;; Experiment for sprite muxing:
+     (tya)
+;;     (clc)
+     (adc (imm (ash (- 11 1) 2)))        ; Subtract one, because we already incremented..
+     (sta *oamidx*)
+     ;; ..instead of..
+;;     (sty *oamidx*)
      (rts)))
 
 (defsprite (sage f 0)
@@ -101,6 +110,7 @@
   (cld)
   (poke #b00010000 +ppu-cr1+)         ; NMI off during init.
   (poke #b00000000 +ppu-cr2+)         ; Do turn the screen off too..
+  (poke #x40 +papu-irq-ctrl+)
   (ldx (imm #xFF))                    ; Set stack pointer
   (txs)
 
@@ -108,51 +118,33 @@
   (poke 0 #x4015)                     ; Silence all channels
   (poke #x40 #x4017)                  ; Disable IRQ
 
+  (bita (mem +ppu-status+))           ; clear vblank flag (reset glitch)
   (as/until :negative (bita (mem +ppu-status+))) ; PPU warmup interval
   (as/until :negative (bita (mem +ppu-status+))) ; (two frames)
 
-  (poke 0 vblank-flag)
   (lda (mem +ppu-status+))            ; Clear vblank flag before enabling NMI!
   (poke #b10001000 +ppu-cr1+)         ; Enable NMI
 
   ;; Reset background
-  (jsr 'wait-for-vblank)
-  ;; Fill first name table screen
-  (lda (mem +ppu-status+))            ; Reset address latch
-  (ppuaddr #x2000)
-  (lda (imm 4))
-  (ldy (imm 8))
-  (as/until :zero
-    (ldx (imm 0))
-    (as/until :zero
-      (sta (mem +vram-io+))
-      (dex))
-    (dey))
-  ;; Fill second screen
-  (ppuaddr #x2400)
-  (lda (imm 5))
-  (ldy (imm 8))
-  (as/until :zero
-    (ldx (imm 0))
-    (as/until :zero
-      (sta (mem +vram-io+))
-      (dex))
-    (dey))
+  (jsr 'wait-for-vblank)                ; Rendering is off but just to be safe.. (?..)
+  (jsr 'reset-background)
 
   ;; Palette init
   (jsr 'wait-for-vblank)
   (ppuaddr #x3F00)
   (let ((bg #x1B))
-    (dolist (color (list bg #x2D #x3D #x30  bg #x03 #x13 #x23
+    (dolist (color (list bg #x1D #x3D #x30  bg #x03 #x13 #x23
                          bg #x2D #x3D #x30  bg #x05 #x15 #x25
                          bg #x1d #x15 #x37  bg #x1d #x13 #x37))
      (poke color +vram-io+)))
+
+  (jsr 'reset-frame-vectors)
 
   ;; Main loop - wait for vblank, reset PPU registers, do sprite DMA.
   (with-label :loop
     ;; FIXME: sprite chr table address behaving backward vs what I expect..
     (poke #b10010000 *shadow-cr1*)
-    (poke #b00011100 *shadow-cr2*)
+    (poke #b00011110 *shadow-cr2*)
     (poke 0 *shadow-scroll-x*)
     (poke 0 *shadow-scroll-y*)
 
@@ -191,40 +183,90 @@
      :else
      (jsr '(sage f 1)))
 
-    ;; (poke 120 *sprite-x*)
-    ;; (jsr '(wiz f 0))
-
-
-
-    ;; (poke 80 *sprite-y*)
-
-    ;; (poke 20 *sprite-x*)
-    ;; (jsr '(wiz f 0))
-
-    ;; (poke 40 *sprite-x*)
-    ;; (jsr '(sage f 0))
-
-    ;; (poke 80 *sprite-x*)
-    ;; (jsr '(wiz f 0))
-
-    ;; (poke 100 *sprite-x*)
-    ;; (jsr '(sage f 0))
-
-    ;; (poke 120 *sprite-x*)
-    ;; (jsr '(wiz f 0))
-
-    ;; (poke 140 *sprite-x*)
-    ;; (jsr '(wiz f 0))
-
-    ;; (poke 160 *sprite-x*)
-    ;; (jsr '(wiz f 0))
-
-    ;; (poke 180 *sprite-x*)
-    ;; (jsr '(wiz f 0))
+    (pokeword (label 'test-status-update) status-update-vector)
 
     (jsr 'end-frame)
 
     (jmp (mem :loop))))
+
+(procedure reset-background
+  ;; Fill first name table screen
+  (lda (mem +ppu-status+))            ; Reset address latch
+  (ppuaddr #x2000)
+  (lda (imm 0))
+  (ldy (imm 4))
+  (as/until :zero
+    (ldx (imm 0))
+    (as/until :zero
+      (sta (mem +vram-io+))
+      (dex))
+    (dey))
+  (ppuaddr #x23C0)                      ; Attribute table
+  (lda (imm 0))
+  (ldx (imm 64))
+  (as/until :zero
+    (sta (mem +vram-io+))
+    (dex))
+  ;; Fill second screen
+  (ppuaddr #x2400)
+  (lda (imm 3))                         ; Distinguish color since it should be hidden now
+  (ldy (imm 4))
+  (as/until :zero
+    (ldx (imm 0))
+    (as/until :zero
+      (sta (mem +vram-io+))
+      (dex))
+    (dey))
+  (ppuaddr #x27C0)                      ; Attribute table
+  (lda (imm 0))
+  (ldx (imm 64))
+  (as/until :zero
+    (sta (mem +vram-io+))
+    (dex))
+  (jsr 'redraw-borders)
+  (rts))
+
+(procedure redraw-borders
+  (flet ((draw (x y repeat value)
+           (assert (<= 0 x 31))
+           (assert (<= 0 y 29))
+           (assert (< 0 repeat 256))
+           (ppuxy x y)
+           (cond
+             ((= 1 repeat)
+              (poke value +vram-io+))
+             (t
+              (lda (imm value))
+              (ldx (imm repeat))
+              (as/until :zero
+                (sta (mem +vram-io+))
+                (dex))))))
+    (poke #x00 +ppu-cr1+)               ; Horizontal write mode
+    (lda (mem +ppu-status+))
+    ;; Status bar
+    (draw 0 0 32 1)
+    (draw 0 1 32 5)
+    (draw 0 3 32 4)
+
+    (poke #x84 +ppu-cr1+)               ; Vertical write mode
+    (draw 0 0 30 1)
+    (draw 1 0 30 1)
+    (draw 30 0 30 1)
+    (draw 31 0 30 1)
+
+    ;;(draw 1 4 26 #x10)
+
+    (poke #x80 +ppu-cr1+)               ; Return to horizontal write mode
+
+    ;; Bevel edges of status bar
+    (draw 1 1 1 11)
+    (draw 1 2 1 8)
+    (draw 1 3 1 7)
+    (draw 30 1 1 12)
+    (draw 30 2 1 9)
+    (draw 30 3 1 10)
+    )
+  (rts))
 
 (procedure end-frame
   "Complete processing for one frame. Move sprites, do pending VRAM writes, etc."
@@ -238,8 +280,29 @@
   (poke *shadow-cr1* +ppu-cr1+)
   (poke *shadow-cr2* +ppu-cr2+)
   (lda (mem +ppu-status+))                  ; Reset address latch
+
+  (jmp status-update-vector)
+  (set-label :continue-from-status-update) ; Default (idle) handler jumps here to try next vector
+
+
+  (set-label :finish-vram-updates)      ; If we update VRAM, handler jumps here (we may be out of cycles for additional updates)
+  (lda (mem +ppu-status+))                  ; Reset address latch
   (poke *shadow-scroll-x* +vram-scroll+)
   (poke *shadow-scroll-y* +vram-scroll+)
+  (rts)
+
+  (set-label 'no-status-update *global-context*)
+  (jmp (mem :continue-from-status-update))
+
+  (set-label 'test-status-update *global-context*)
+  (ppuxy 2 2)
+  (loop for code across (map-string "Hi there.") do (poke code +vram-io+))
+  (pokeword (label 'no-status-update) status-update-vector)
+  (jmp (mem :continue-from-status-update)))
+
+(procedure reset-frame-vectors
+  "Call this before first call to END-FRAME, to initialize shadows and vectors."
+  (pokeword (label 'no-status-update) status-update-vector)
   (rts))
 
 (procedure reset-sprites
@@ -262,6 +325,20 @@
   (inc *frame-counter*)
   (plp)
   (rti))
+
+(defparameter *character-mapping*
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()`~-_+=[]{};:'\",.<>/?\\| ")
+
+(defun map-string (string)
+  (map 'vector (lambda (char) (+ #xA0 (position char *character-mapping*))) string))
+
+
+
+;;;; End of program
+
+(print (list :program-end *origin*
+             :space-used (- *origin* #x8000)
+             :space-remaining (- #xFFFA *origin*)))
 
 ;;;; Interrupt vectors
 
