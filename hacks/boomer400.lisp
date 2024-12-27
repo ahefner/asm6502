38a0ae2f804d0a423a7a84039b9ac81b4efa9294
Andy Hefner <ahefner@gmail.com>
2024-12-27 13:09:44 -0500

    Flailing around getting a feel for Atari programming

--- hacks/boomer400.lisp
+++ hacks/boomer400.lisp
@@ -9,44 +9,137 @@
 (defconstant COLBK #xD01A)
 (defconstant SKCTL #xD20F)
 (defconstant DMACTL #xD400)
+(defconstant SDMCTL #x22f)
+(defconstant PMBASE #xD407)
+(defconstant CHBAS 756)
 
+(defconstant SDLIST #x230)
 (defconstant DLIST #xD402)		; ...
 (defconstant DLISTL #xD402)
 (defconstant DLISTH #xD403)
 
+(defconstant CHBASE #xD409)
 
-
-;;; Hello?
 (setf
  (binary-file "/tmp/boomer400.car")
  (let* ((global (make-instance 'basic-context :address #xA000))
 	(*context* global))
+#+NIL
+   (db #b00000101
+       #b00000101
+       #b00000101
+       #b00000101
+       #b10101111
+       #b10101111
+       #b10101111
+       #b10101111)
 
+   (emit
+    (ichr:append-rows
+     (ichr:swizzle-rows 16
+      (ichr:chunkify 2 8
+       (ichr:read-gif (merge-pathnames "boomer3.gif" *path*))))))
+   
+   (db #b10000000
+       #b11000000
+       #b11100000
+       #b11110000
+       #b11111000
+       #b11111100
+       #b11111110
+       #b11111111 )
+   
+   (set-label :start)
    (poke 0 SKCTL)
    (poke 3 SKCTL)
-   (poke #x22 DMACTL)
-   (pokeword (label 'display-list) DLIST)
+;;   (poke #x22 DMACTL)
+;;   (poke #x3e SDMCTL)
+   
+   (poke 0 SDMCTL)
+   (poke #x00 PMBASE)
+   (poke #xA0 CHBAS)
+
+
+   (poke 0 160)
+   (poke 1 200)
+   (poke 2 161)
+   (poke 3 201)
+   
+   (poke 8 140)
+   (poke 9 180)
+   (poke 10 141)
+   (poke 11 181)
+
+   (poke 12 144)
+   (poke 13 184)
+   (poke 14 145)
+   (poke 15 185)
+
+   (poke 4 148)
+   (poke 5 188)
+   (poke 6 149)
+   (poke 7 189)
+
+
+   ;;(pokeword (label 'display-list) DLIST)
+   (pokeword (label 'display-list) SDLIST)
+   (poke #x22 SDMCTL)
+
+   (poke #x56 (zp 77))
+   ;;(poke #x56 (mem COLBK))
+   ;; (poke #x07 708)			; Playfield colors
+   ;; (poke #x10 709)
+   ;; (poke #x3c 710)
+   ;; (poke #x57 711)
+
+   ;;(poke #xD6 712)
+   (poke #x08 712)
+   (poke #x3A 708)
+   ;;(poke #x08 709)
+   (poke #xD6 709)
+   (poke #x00 710)
+   (poke #xFE 711)
+   
+   
+
 
    (set-label :loop)
-   (lda (zp 77))
-   (sta (mem COLBK))
-   (inc (zp 77))
 
+   ;; (lda (zp 77))
+   ;; (sta (mem COLBK))
+   ;; (inc (zp 77))
+   ;; (nop)
+   ;; (nop)
+   ;; (nop)
+   ;; (nop)
+   (jmp (mem #xe471))
    (jmp (mem :loop))
 
    (with-label display-list
-;;     (db #x70 #x70 #x70 #x70 #x70 #x70 #x70 #x70 #x70 #x70 #x70 #x70 #x70)
-     (db #x70 #x70 #x70)
-     (db #x42)
-     (dw (label 'message))
-     (loop repeat 23 do (db #x02))
+     ;; (db #x70 #x70 #x70 #x42)
+     ;; ;; #x00 #x38
+     ;; ;;(dw (label 'message))
+     ;; (dw 0)
+     ;; (db #x02 #x02 #x02 #x02 #x82 #x70 #x86 #x70 #x70 #x02
+     ;; 	 #x70 #x07 #x70 #x30 #x06 #x70 #x06 #x70 #x30 #x06 #x70 #x70 #x02)
+
+     ;; Plain text mode display list
+     (db #x70 #x70 #x70 #x44)
+     ;;(dw (label 'message))
+     ;;(dw #x3c40)
+     (dw 0)
+     (loop repeat 23 do (db #x04))
      (db #x41)
      (dw (label 'display-list)))
 
    (with-label message
+     (loop for i from 0 below 256 do (db i))
      (emit (map 'list 'char-code "  YOUR ATARI WORKS  "))
      (emit (loop repeat 20 collect #x20)))
-   
+
+   (with-label :init
+     (rts))
+
    (advance-to #xBFFA)
-   (dw #xA000 #x8000 #xA000)
+   (dw (label :start) #x0400 (label :init))
    (link global)))
--- ichr.lisp
+++ ichr.lisp
@@ -2,7 +2,10 @@
   (:use common-lisp)
   (:import-from asm6502 binary-file)
   (:export #:decode-chr #:encode-chr #:encode-gif
-           #:write-gif #:read-gif))
+           #:write-gif #:read-gif
+	   #:chunkify
+	   #:swizzle-rows
+	   #:append-rows))
 
 (in-package :ichr)
 
@@ -73,25 +76,84 @@
                        (skippy:height img))
      (skippy:color-table ds))))
 
+;;; Encode 2bpp 8x8 characters into NES bitplaned format
 (defun encode-chr (pixels)
-  (loop ;with sheet-width = (truncate (array-dimension pixels 1) 8)
-        ;with sheet-height = (truncate (array-dimension pixels 0) 8)
-        with output = (make-array ;;(* 16 sheet-width sheet-height)
-                       0
-                       :element-type '(unsigned-byte 8)
-                       :fill-pointer t
-                       :adjustable t)
-        for y from 0 below (array-dimension pixels 0) by 8 ;sheet-height
-        do
-        (loop for x from 0 below (array-dimension pixels 1) by 8;sheet-width
-              do
-              (dotimes (plane 2)
-                (dotimes (oy 8)
-                  (vector-push-extend
-                   (loop for ox from 0 below 8 summing (ash (ldb (byte 1 plane) (aref pixels (+ y oy) (+ x (- 7 ox)))) ox))
-                   output))))
-        finally (return output)))
+  (loop with output = (make-array
+		       0
+		       :element-type '(unsigned-byte 8)
+		       :fill-pointer t
+		       :adjustable t)
+          for y from 0 below (array-dimension pixels 0) by 8 ;sheet-height
+          do
+             (loop for x from 0 below (array-dimension pixels 1) by 8 ;sheet-width
+		   do
+		      (dotimes (plane 2)
+			(dotimes (oy 8)
+			  (vector-push-extend
+			   (loop for ox from 0 below 8 summing (ash (ldb (byte 1 plane) (aref pixels (+ y oy) (+ x (- 7 ox)))) ox))
+			   output))))
+          finally (return output)))
 
 (defun encode-gif (pathname)
   "Helper function: Equivalent to (encode-chr (read-gif pathname))"
   (encode-chr (read-gif pathname)))
+
+;;;; These are slightly more general utilities which I'm writing for the Atari
+;;;; 8-bit but apply to some other chunky pixel situations.
+
+(defun combine-pair (x y nbits)
+  (dpb (ldb (byte nbits 0) x)
+       (byte nbits nbits)
+       (ldb (byte nbits 0) y)))
+
+(defun chunkify-neighbors (nbits matrix)
+  (assert (evenp (array-dimension matrix 1)))
+  (let ((output (make-array (list (array-dimension matrix 0)
+				  (ash (array-dimension matrix 1) -1)))))
+    (dotimes (row (array-dimension matrix 0))
+      (dotimes (col (array-dimension output 1))
+	(setf (aref output row col)
+	      (combine-pair (aref matrix row (* col 2))
+			    (aref matrix row (1+ (* col 2)))
+			    nbits))))
+    output))
+
+(defun is-power-of-two? (x)
+  (and (integerp x)
+       (> x 0)
+       (zerop (logand x (1- x)))))
+
+(defun chunkify (bpp byte-width matrix)
+  (assert (is-power-of-two? bpp))
+  (assert (<= bpp byte-width))
+  (if (= bpp byte-width)
+      matrix
+      (chunkify
+       (* 2 bpp)
+       byte-width
+       (chunkify-neighbors bpp matrix))))
+
+;;; This will rearrange groups of 1 column by 'tile-height' row
+;;; character cells in a 2D image such that the rows of the character
+;;; are now adjacent columns in the output. Output width is multiplied
+;;; by tile-height, height is divided by tile-height.
+(defun swizzle-rows (tile-height matrix)
+  (assert (zerop (mod (array-dimension matrix 0) tile-height)))
+  (loop with output = (make-array (list (/ (array-dimension matrix 0) tile-height)
+					(* (array-dimension matrix 1) tile-height)))
+	for row upfrom 0
+	for y0 below (array-dimension matrix 0) by tile-height 
+	do
+	   (loop for x0 from 0 below (array-dimension matrix 1) do
+	     (loop for i from 0 below tile-height do
+	       (setf (aref output row (+ i (* x0 tile-height)))
+		     (aref matrix (+ y0 i) x0))))
+	finally (return output)))
+
+(defun append-rows (matrix)
+  (loop with n = (array-total-size matrix)
+	with result = (make-array n)
+	for i below n
+	do (setf (aref result i) (row-major-aref matrix i))
+	finally (return result)))
+
